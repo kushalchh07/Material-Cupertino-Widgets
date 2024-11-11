8e2e3525357e26ad6ac44fa18181b74675c13de6
kushalchh07 <kushalchhetri54@gmail.com>
2024-11-11 11:16:17 +0545

    fet: small example of cupertino app and the material app added

---
+++ lib/pages/home/homepage.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CupertinoButton(
+            onPressed: () {},
+            child: const Text("Cupertino Button"),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Material Button"),
+          )
+        ],
+      ),
+    ));
+  }
+}
+
+// Widget adaptiveButton(String text, VoidCallback onPressed) {
+//   if (Platform.isIOS) {
+//     return CupertinoButton(
+//       onPressed: onPressed,
+//       child: Text(text),
+//     );
+//   } else {
+//     return ElevatedButton(
+//       onPressed: onPressed,
+//       child: Text(text),
+//     );
+//   }
+// }
